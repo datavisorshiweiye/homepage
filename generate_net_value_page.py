a1c3a5aae705f07e75a9bdd271dcd632c008df41
Shiwei Ye <shiwei.ye@datavisor.com>
2020-07-07 20:54:58 +0800

    Add draw down plot.

--- generate_net_value_page.py
+++ generate_net_value_page.py
@@ -8,9 +8,18 @@ def make():
     df['net_value'] = [round(a, 3) for a in df['net_value']]
     date_str = str(list([d.date().strftime('%Y-%m-%d') for d in df['date']]))
     data_str = str(list(df['net_value']))
+    draw_down = [0]
+    net_value = list(df['net_value'])
+    pre_max = net_value[0]
+    for i in range(1, len(net_value)):
+        if net_value[i] > pre_max:
+            pre_max = net_value[i]
+        draw_down.append(round(-100*(1-net_value[i]/pre_max), 3))
+    draw_down_str = str(draw_down)
     template = open('net_value_template.html').read()
     template = template.replace('dates_pos', date_str)
     template = template.replace('net_value_pos', data_str)
+    template = template.replace('draw_down_pos', draw_down_str)
     fp = open('net_value.html','w')
     fp.write(template)
     fp.close()
